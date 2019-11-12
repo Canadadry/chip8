f1314b84d0323c969e9ee386297736fcec7096ec
Mourey jerome <jerome@everycheck.fr>
2019-11-12 13:00:44 +0100

    build passing

--- src/chip8/cpu.rs
+++ src/chip8/cpu.rs
@@ -1,7 +1,7 @@
 use super::instruction::Instruction;
 
 const MEMORY_SIZE:usize    =  4096;
-const START_PC:u32         =  0x200;
+const START_PC:u16         =  0x200;
 const REGISTER_COUNT:usize =  16;
 const STACK_SIZE:usize     =  16;
 
@@ -14,7 +14,8 @@ pub struct Cpu
 	sp:u8,
 	dt:u8,
 	st:u8,
-	pc:u16
+	pc:u16,
+	seed:u32,
 }
 
 impl Cpu
@@ -30,14 +31,15 @@ impl Cpu
 			dt:0u8,
 			st:0u8,
 			pc:0u16,
+			seed:0u32
 		}
 	}
 	
 	pub fn reset(&mut self)
 	{
-		for i in 0..self.m.len() { self.m[i] = 0u16; }
-		for i in 0..self.c.len() { self.c[i] = 0u16; }
-		for i in 0..self.s.len() { self.s[i] = 0u32; }
+		for i in 0..self.m.len() { self.m[i] = 0u8; }
+		for i in 0..self.v.len() { self.v[i] = 0u8; }
+		for i in 0..self.s.len() { self.s[i] = 0u16; }
 		
 		self.i=0;
 		self.sp = 0;
@@ -45,43 +47,132 @@ impl Cpu
 		self.st = 0;
 		self.pc= START_PC;
 	}
-	
-	pub fn step_timer(&mut self)
-	{
-		if self.dt>0 { self.dt-=1; }
-		if self.st>0 { self.st-=1; }
-	}
-	
-	pub fn get_opcode(&self) -> u32
-	{
-		return  ((self.m[self.pc as usize    ] as u32 ) << 16) 
-		       | (self.m[self.pc as usize + 1]  as u32);
-	}
-	
+
 	pub fn load(&mut self,rom:&Vec<u8>)
 	{
 		self.reset();
-		for i in 0..rom.len()/2
+		for i in 0..rom.len()
 		{
-			self.m[i] = (rom[2*i] as u16) << 8 | rom[2*i+1] as u16;
+			self.m[i + START_PC as usize] = rom[i];
 		}			
 	}
-
-	pub fn fetch() -> u16
+	
+	pub fn step(&mut self)
 	{
+		let op = self.fetch();
+		let it = Instruction::decode(op);
+		self.execute(&it);
 
+		if self.dt>0 { self.dt-=1; }
+		if self.st>0 { self.st-=1; }
 	}
 
-	pub fn decode(opcode:u32) -> Instruction
+
+	fn fetch(&mut self) -> u16
 	{
-		Instruction{ 
-			op_1   : (opcode & 0xF000) >> 12,
-			op_2   : (opcode & 0x0F00) >> 8,
-			op_3   : (opcode & 0x00F0) >> 4,
-			op_4   :  opcode & 0x000F,
-			op_234 :  opcode & 0x0FFF,
-			op_34  : (opcode & 0x00FF)
-    	}
+		let msb    = self.m[self.pc as usize    ] as u16;
+		let lsb    = self.m[self.pc as usize + 1] as u16;
+		let opcode = msb << 8 | lsb;
+		self.pc += 2;
+
+		return opcode;
 	}
 	
+	fn execute(&mut self, instruction:&Instruction) 
+	{
+		match instruction.op_1 {
+			1=> {
+				self.pc = instruction.op_234;
+			},
+			2 => {
+				if self.sp >= 16 {panic!("Stack Overflow");}
+				self.s[self.sp as usize] = self.pc;
+				self.sp+=1;
+			},
+			3 => {
+				if self.v[instruction.op_2 as usize] == instruction.op_34 {
+					self.pc += 2;
+				}
+			},
+			4 => {
+				if self.v[instruction.op_2 as usize] != instruction.op_34 {
+					self.pc += 2;
+				}
+			}, 
+			5 => {
+				if self.v[instruction.op_2 as usize] == self.v[instruction.op_3 as usize] {
+					self.pc += 2;
+				}
+			}, 
+			6 => {
+				self.v[instruction.op_2 as usize] = instruction.op_34;
+			}, 
+			7 => {
+				self.v[instruction.op_2 as usize] += instruction.op_34;
+			}, 
+			8 => {
+				match instruction.op_4 {
+					0 => {
+						self.v[instruction.op_2 as usize] = self.v[instruction.op_3 as usize];
+					},
+					1 => {
+						self.v[instruction.op_2 as usize] |= self.v[instruction.op_3 as usize];
+					},
+					2 => {
+						self.v[instruction.op_2 as usize] &= self.v[instruction.op_3 as usize];
+					},
+					3 => {
+						self.v[instruction.op_2 as usize] = self.v[instruction.op_3 as usize];
+					},
+					4 => {
+						self.v[instruction.op_2 as usize] += self.v[instruction.op_3 as usize];
+						// todo overflow and carry in vf
+					},
+					5 => {
+						self.v[instruction.op_2 as usize] -= self.v[instruction.op_3 as usize];
+						// todo overflow and carry in vf cf doc not borrow
+					},
+					6 => {
+						self.v[instruction.op_2 as usize] <<= 1;
+						// todo overflow and carry in vf
+					},
+					7 => {
+						self.v[instruction.op_2 as usize] = self.v[instruction.op_3 as usize] - self.v[instruction.op_2 as usize];
+						// todo overflow and carry in vf
+					},
+					0xE => {
+						self.v[instruction.op_2 as usize] >>= 1;
+						// todo overflow and carry in vf
+					}
+					_ => {
+						panic!("Not opcode");
+					}
+				}
+			},
+			9 => {
+				if self.v[instruction.op_2 as usize] != self.v[instruction.op_3 as usize] {
+					self.pc += 2;
+				}
+			},
+			0xA => {
+				self.i = instruction.op_234;
+			},
+			0xB => {
+				self.pc = instruction.op_234 + self.v[0] as u16;
+			},
+			0xC => {
+				self.v[instruction.op_2 as usize] = self.rand() & instruction.op_34;
+			}
+			_ => {
+				panic!("Not opcode");
+			}
+		}
+	}
+
+	fn rand(&mut self) -> u8
+	{
+	    self.seed = (self.seed * 1103515245 + 12345) % std::u32::MAX;
+	    return (self.seed & 0xFFu32) as u8;
+	}
+
 }
--- src/chip8/instruction.rs
+++ src/chip8/instruction.rs
@@ -2,110 +2,25 @@ use super::cpu::Cpu;
 
 pub struct Instruction
 {
-	op_1:u8,
-	op_2:u8,
-	op_3:u8,
-	op_4:u8,       
-	op_234:u16,
-	op_34:u8,
+	pub op_1:u8,
+	pub op_2:u8,
+	pub op_3:u8,
+	pub op_4:u8,       
+	pub op_234:u16,
+	pub op_34:u8,
 }
 
 impl Instruction{
 
-	fn from_opcode(opcode:u16) -> Instruction
+	pub fn decode(opcode:u16) -> Instruction
 	{
 		Instruction{ 
-			op_1   : (opcode & 0xF000) >> 12,
-			op_2   : (opcode & 0x0F00) >> 8,
-			op_3   : (opcode & 0x00F0) >> 4,
-			op_4   :  opcode & 0x000F,
-			op_234 :  opcode & 0x0FFF,
-			op_34  : (opcode & 0x00FF)
-		}
-	}
-	fn runOn(&self, cpu:&mut CPU) 
-	{
-		match self.op_1 {
-			1=> {
-				cpu.pc = op_234;
-			},
-			2 => {
-				if cpu.sp >= 16 {panic!("Stack Overflow");}
-				cpu.s[cpu.sp] = cpu.pc;
-				cpu.sp++;
-			},
-			3 => {
-				if cpu.v[self.op_2] == self.op_34 {
-					cpi.pc += 2;
-				}
-			},
-			4 => {
-				if cpu.v[self.op_2] != self.op_34 {
-					cpi.pc += 2;
-				}
-			}, 
-			5 => {
-				if cpu.v[self.op_2] == cpu.v[self.op_3] {
-					cpi.pc += 2;
-				}
-			}, 
-			6 => {
-				cpu.v[self.op_2] = self.op_34;
-			}, 
-			7 => {
-				cpu.v[self.op_2] += self.op_34;
-			}, 
-			8 => {
-				match self.op_4 {
-					0 => {
-						cpu.v[self.op_2] = cpu.v[self.op_3];
-					},
-					1 => {
-						cpu.v[self.op_2] |= cpu.v[self.op_3];
-					},
-					2 => {
-						cpu.v[self.op_2] &= cpu.v[self.op_3];
-					},
-					3 => {
-						cpu.v[self.op_2] = cpu.v[self.op_3];
-					},
-					4 => {
-						cpu.v[self.op_2] += cpu.v[self.op_3];
-						// todo overflow and carry in vf
-					},
-					5 => {
-						cpu.v[self.op_2] -= cpu.v[self.op_3];
-						// todo overflow and carry in vf cf doc not borrow
-					},
-					6 => {
-						cpu.v[self.op_2] <<= 1;
-						// todo overflow and carry in vf
-					},
-					7 => {
-						cpu.v[self.op_2] = cpu.v[self.op_3] - cpu.v[self.op_2];
-						// todo overflow and carry in vf
-					},
-					0xE => {
-						cpu.v[self.op_2] >>= 1;
-						// todo overflow and carry in vf
-					},
-				},
-				9 => {
-					if cpu.v[self.op_2] != cpu.v[self.op_3] {
-						cpi.pc += 2;
-					}
-				},
-				0xA => {
-					cpu.i = self.op_234;
-				},
-				0xB => {
-					cpu.pc = self.op_234 +  cpu.v[0];
-				},
-				0xC => {
-					cpu.v[self.op_2] = 0;
-					// implement rand
-				}
-			}, 
+			op_1   : ( ((opcode & 0xF000u16) >> 12)  as u8 ),
+			op_2   : ( ((opcode & 0x0F00u16) >> 8 )  as u8 ),
+			op_3   : ( ((opcode & 0x00F0u16) >> 4 )  as u8 ),
+			op_4   : (  (opcode & 0x000Fu16)         as u8 ),
+			op_234 : (  (opcode & 0x0FFFu16)               ),
+			op_34  : (  (opcode & 0x00FFu16)         as u8 )
 		}
 	}
 }
